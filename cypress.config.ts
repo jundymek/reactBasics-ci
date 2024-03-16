242c8d686dea0e55f1c3dfb3d9a45c89535f8383
jundymek <jundymek@gmail.com>
2024-03-16 22:29:47 +0100

    chore: Update baseUrl in cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "cypress"
 
+// Determine the environment using an environment variable, defaulting to 'development'
+const environment = process.env.NODE_ENV || "development"
+
+// Set the baseUrl based on the environment
+const baseUrl = environment === "development" ? "http://localhost:5173" : "http://localhost:4173"
+
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173",
+    baseUrl,
     specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
