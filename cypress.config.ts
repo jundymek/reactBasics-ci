6b0abd5e0c0a8681ce288c573c833dabe71dbf7a
jundymek <jundymek@gmail.com>
2024-03-16 22:35:32 +0100

    chore: Update preview server port and Cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "cypress"
 
-// Determine the environment using an environment variable, defaulting to 'development'
-const environment = process.env.NODE_ENV || "development"
-
-// Set the baseUrl based on the environment
-const baseUrl = environment === "development" ? "http://localhost:5173" : "http://localhost:4173"
-
 export default defineConfig({
   e2e: {
-    baseUrl,
+    baseUrl: "http://localhost:5173",
     specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { sentryVitePlugin } from "@sentry/vite-plugin"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jundymek",
-    project: "javascript-react"
-  })],
+  preview: {
+    port: 5173,
+  },
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "jundymek",
+      project: "javascript-react",
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
 })
